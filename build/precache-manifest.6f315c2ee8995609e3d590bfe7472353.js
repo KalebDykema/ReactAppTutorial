8fa4a189496f6c9092223385e52a533076d89f57
Kaleb Dykema <dykemakaleb@gmail.com>
2020-08-31 22:02:37 -0500

    Fixing deploy issue with Router

---
+++ build/precache-manifest.6f315c2ee8995609e3d590bfe7472353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a358d5059decb077782eb79c01718b17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9427b54819b166de0179",
+    "url": "/static/css/main.6993a495.chunk.css"
+  },
+  {
+    "revision": "8a3bb23031a29928602d",
+    "url": "/static/js/2.5e548879.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5e548879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9427b54819b166de0179",
+    "url": "/static/js/main.cb1086a0.chunk.js"
+  },
+  {
+    "revision": "d89ae92fc598edbb697f",
+    "url": "/static/js/runtime-main.a787cda0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d396af73ca747070e72a4f8aa72eb3d6.js"
+  "/precache-manifest.6f315c2ee8995609e3d590bfe7472353.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
